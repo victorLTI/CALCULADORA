d1b9fdca1ba7e91c0d2ac2fd428e294cfce26226
victor gl <victor.garzaly@uanl.edu.mx>
2022-10-28 16:25:40 -0500

    cree primera sentencia y for de impresionde  tabla normal

--- tablas.js
+++ tablas.js
@@ -3,7 +3,10 @@ let long=parseInt(document.getElementById("long").value);
 let tabla = document.getElementById('tabla').value;
 let tipo = document.getElementById("tipo").value;
 
-    if(){
+    if(tipo==1){
+    for (let i = 0; i < long; i++) {
+        document.write(tabla+" X "+i+" = "+(i*tabla)+"<br>");
 
+    }
     }
 }
