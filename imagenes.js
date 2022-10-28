d772a172810d247a711817fee101db8df4321a56
victor gl <victor.garzaly@uanl.edu.mx>
2022-10-28 18:04:21 -0500

    se modifico la ruta de las imagenes

--- imagenes.js
+++ imagenes.js
@@ -1,15 +1,11 @@
 function imagen(){
     let op = parseInt(document.getElementById("num").value);
     let img = document.getElementById("imagen");
-    switch (option) {
-        case 1:
-            img.src="EVIDENCIA-3/recursos/tilin.jpg";
-            break;
-        case 2:
-            img.src="EVIDENCIA-3/recursos/tilin2.jpg";
-            break;
-        case 3:
-            img.src="EVIDENCIA-3/recursos/tilin3.jpeg";
-            break;
-}
+if(op==1){
+    imagen.value=img.src="recursos/tilin.jpg";}
+else if(op==2){
+    imagen.value=img.src="recursos/tilin2.jpg";}
+else if(op==3){
+    imagen.value=img.src="recursos/tilin3.jpeg";}
+
 }
