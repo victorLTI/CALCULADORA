0e54de67267a4a3d7ed9cf79585998e64a530bb1
victor gl <victor.garzaly@uanl.edu.mx>
2022-10-28 16:24:23 -0500

    cree let tabla y let tipo

--- tablas.js
+++ tablas.js
@@ -1,4 +1,6 @@
 function Generar() {
 let long=parseInt(document.getElementById("long").value);
+let tabla = document.getElementById('tabla').value;
+let tipo = document.getElementById("tipo").value;
 
 }
