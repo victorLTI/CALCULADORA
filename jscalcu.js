d6a9c7164a1fc34ece3449aef6dccd4026cd4e7e
victor gl <victor.garzaly@uanl.edu.mx>
2022-10-28 00:51:23 -0500

    cree funcion para calcular

---
+++ jscalcu.js
@@ -0,0 +1,5 @@
+function calcu(){
+    let num1=parseInt(document.getElementById("num1").value);
+    let num2=parseInt(document.getElementById("num2").value);
+    let op=parseInt(document.getElementById("select").value);
+    let resul=document.getElementById("resul");
