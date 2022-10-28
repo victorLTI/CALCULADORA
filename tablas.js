30020b6933340f541223c8fc5ce92265601cccd4
victor gl <victor.garzaly@uanl.edu.mx>
2022-10-28 16:26:09 -0500

    cree elseif para imprimir tablas inversas y termine

--- tablas.js
+++ tablas.js
@@ -6,7 +6,11 @@ let tipo = document.getElementById("tipo").value;
     if(tipo==1){
     for (let i = 0; i < long; i++) {
         document.write(tabla+" X "+i+" = "+(i*tabla)+"<br>");
-
     }
+    }
+    else if(tipo==2){
+    for(let i = long; i >= 1; i--) {
+        document.write(i+"X"+tabla+ "="+ (i*tabla)+"<br>");
+        }
     }
 }
