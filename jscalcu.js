afbc6a1cefdea9a757cac2570af5d222535fc64d
victor gl <victor.garzaly@uanl.edu.mx>
2022-10-28 00:52:07 -0500

    cree ciclo if de decision

--- jscalcu.js
+++ jscalcu.js
@@ -3,3 +3,7 @@ function calcu(){
     let num2=parseInt(document.getElementById("num2").value);
     let op=parseInt(document.getElementById("select").value);
     let resul=document.getElementById("resul");
+    
+    if(op==1){
+        resul.value=num1+num2;
+    }
