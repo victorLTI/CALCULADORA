5f4fd9b0101be42d379089d441b643296e08a8c7
victor gl <victor.garzaly@uanl.edu.mx>
2022-10-28 01:50:59 -0500

    se creo ciclo switch para seleccionar una imagen

--- imagenes.js
+++ imagenes.js
@@ -1,4 +1,15 @@
 function imagen(){
-    let option = parseInt(document.getElementById("num").value);
-    let imga = document.getElementById("imagen");
+    let op = parseInt(document.getElementById("num").value);
+    let img = document.getElementById("imagen");
+    switch (option) {
+        case 1:
+            img.src="Recursos/tilin.jpg";
+            break;
+        case 2:
+            img.src="Recursos/tilin2.jpg";
+            break;
+        case 3:
+            img.src="Recursos/tilin3.jpeg";
+            break;
+}
 }
