3689a102bcfd68f9ecdff330a06d015864174849
victor gl <victor.garzaly@uanl.edu.mx>
2022-10-28 16:24:56 -0500

    cree if js

--- tablas.js
+++ tablas.js
@@ -3,4 +3,7 @@ let long=parseInt(document.getElementById("long").value);
 let tabla = document.getElementById('tabla').value;
 let tipo = document.getElementById("tipo").value;
 
+    if(){
+
+    }
 }
