cc74644bfc2915516eda97ab0bfcc65053eecc18
victor gl <victor.garzaly@uanl.edu.mx>
2022-10-28 22:06:10 -0500

    se termino js1 js

---
+++ js1.js
@@ -0,0 +1,20 @@
+console.log("Hola mundo");
+console.warn("precaucion");
+console.error("error") 
+
+console.log(`El tipo de dato de false ${typeof false}`);
+
+console.log(`El tipo de dato de 5.5 ${typeof 5.5}`);
+
+console.log(`El tipo de dato de js ${typeof 'js'}`);
+
+console.log(`El resultado de:
+${3>1||3==2}`);
+
+console.log(3>2 ? "verdadero" : "falso");
+console.log(!3>2 ? "verdadero" : "falso");
+
+console.log("v num");
+let edad=19;
+console.log("constante")
+const UNI = "UANL";
