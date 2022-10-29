6eb98eab23f05207a580f24c1454b4ffbfb48a91
victor gl <victor.garzaly@uanl.edu.mx>
2022-10-28 21:41:17 -0500

    se imprime en la consola el resultado de los ciclos

--- tomas.js
+++ tomas.js
@@ -6,4 +6,6 @@ if(total<6){cantidad=6;}
 if(cantidad>13){cantidad=13;}
 for(let i=1; i <=frecuencia; i+=1){
     if(hora>23){hora=hora-24;}
+console.log("toma:"+i+" || hora:"+hora);
+hora=hora+frecuencia;
 }
