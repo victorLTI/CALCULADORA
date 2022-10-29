62b481147ea56af0f25170127816dcd9f6f76fe1
victor gl <victor.garzaly@uanl.edu.mx>
2022-10-28 21:33:29 -0500

    termine el ciclo while y agregue algunos comentarios para no perderme

--- numper.js
+++ numper.js
@@ -1,21 +1,26 @@
 function calcu(){
+//se declaran las variables num(perfectos) y resul(imprimir)//
 let num=parseInt(document.getElementById("num").value);
 let resul=document.getElementById("resul"); 
-let cantidad=0;
- while(contador<num){
-
-    let suma= 0;
-    for(let i=1; i<=num; i++){
-        for(let n=1; n<i;n++){
-            if(i%n==0){
-                suma+=n;
-            }   
-                }
-                if(suma==i){
-                    resul.value=(i)
-                    cantidad++;
+//variables para los ciclos for//
+let cantidad=1;
+let suma= 0;
+let i=1;
+let contador=1;
+//innerhtml que usare referenciado a resul para imprimir los numeros//
+resul.innerHTML="";
+//inicio del contador para saber cuantos numper debe sacar con if anidados//
+ do{
+    for(let n=1; n<i;n++){
+        if(i%n==0){suma+=n;}   
         }
-        suma=0
+    if(suma==i){
+//imprimir resultado
+        resul.innerHTML+=i+", ";
+        contador++
     }
-}
+    suma=0
+    i++
+//fin de ciclo while
+    }while(contador<=num);
 }
