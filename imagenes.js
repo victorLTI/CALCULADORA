976a1c24d472f75abdae59dd00664c23c6aa2ac2
victor gl <victor.garzaly@uanl.edu.mx>
2022-10-28 21:28:21 -0500

    terminado y rutas modificadas

--- imagenes.js
+++ imagenes.js
@@ -1,11 +1,10 @@
-function imagen(){
+function imprimir(){
     let op = parseInt(document.getElementById("num").value);
     let img = document.getElementById("imagen");
 if(op==1){
-    imagen.value=img.src="recursos/tilin.jpg";}
+    imprimir.value=img.src="recursos/tilin.jpg";}
 else if(op==2){
-    imagen.value=img.src="recursos/tilin2.jpg";}
+    imprimir.value=img.src="recursos/tilin2.jpg";}
 else if(op==3){
-    imagen.value=img.src="recursos/tilin3.jpeg";}
-
+    imprimir.value=img.src="recursos/tilin3.jpeg";}
 }
